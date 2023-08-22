6053eff8cb60dd314d6d5bed1cdfcc8393013052
vedsu <90701239+vedsu@users.noreply.github.com>
2023-08-22 14:53:58 +0530

    streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -215,14 +215,14 @@ elif authentication_status == None:
 
 
 
-if __name__=='__main__':
-    current_time = datetime.datetime.now().time()
-    target_time_start = datetime.time(14, 0)
-    target_time_end = datetime.time(15, 0)
-    if target_time_start <= current_time < target_time_end:
-        t1 = threading.Thread(target=extract)
-        t1.start()
-        t1.join()
+# if __name__=='__main__':
+ current_time = datetime.datetime.now().time()
+ target_time_start = datetime.time(14, 0)
+ target_time_end = datetime.time(16, 0)
+ if target_time_start <= current_time < target_time_end:
+     t1 = threading.Thread(target=extract)
+     t1.start()
+     t1.join()
 
 
 
