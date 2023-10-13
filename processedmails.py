9dfcd6f475f22d07b2d3ab04ae66b2e62393e61c
vedsu <90701239+vedsu@users.noreply.github.com>
2023-10-13 16:30:17 +0530

    Update processedmails.py

--- processedmails.py
+++ processedmails.py
@@ -8,14 +8,14 @@ import time
 def init_connection():
    
     try:
-        # db_username = st.secrets.db_username
-        # db_password = st.secrets.db_password
+        db_username = st.secrets.db_username
+        db_password = st.secrets.db_password
 
-        # mongo_uri_template = "mongodb+srv://{username}:{password}@emailreader.elzbauk.mongodb.net/"
-        # mongo_uri = mongo_uri_template.format(username=db_username, password=db_password)
+        mongo_uri_template = "mongodb+srv://{username}:{password}@emailreader.elzbauk.mongodb.net/"
+        mongo_uri = mongo_uri_template.format(username=db_username, password=db_password)
 
-        # client = pymongo.MongoClient(mongo_uri)
-        client = pymongo.MongoClient("mongodb://localhost:27017")
+        client = pymongo.MongoClient(mongo_uri)
+        # client = pymongo.MongoClient("mongodb://localhost:27017")
 
         return client
     
